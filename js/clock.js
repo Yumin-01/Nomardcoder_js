6b7126c9602b7afe92a6c1070d2f4b81e3b3e252
yumin park <qw9720@gmail.com>
2022-10-27 16:34:53 +0900

    2022-10-27(2)

--- js/clock.js
+++ js/clock.js
@@ -7,7 +7,7 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     // clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-    clock.innerText = (`${hours}:${minutes}:${seconds}`);
+    clock.innerText = (`${hours} : ${minutes} : ${seconds}`);
 
 }
 // setInterval(sayHello, 5000); //5s = 5000ms
